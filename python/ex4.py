41c16b057fba23e0ad1a9af48aad52291c51330f
Sherwood <Sherwood@vagelos-ve702-5786.apng.wlan.private.upenn.edu>
2018-06-05 11:53:22 -0400

    new ex

---
+++ python/ex4.py
@@ -0,0 +1,10 @@
+my_info = {
+    'fname': 'Ken',
+    'lname': 'Sherwood',
+    'hometown': 'Indiana',
+    'ppn':"his",
+    'fav_food':'paella'}
+print("This is the my_info dict")
+print(my_info)
+print("This is the first item")
+print(my_info['fname'])
