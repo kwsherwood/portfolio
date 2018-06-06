8775c5184b3b3d6653f008afb5921078b90bd4c3
Kenneth Sherwood <kwsherwood@users.noreply.github.com>
2018-06-06 11:40:02 -0400

    Using .items to iterate through a dictionary.

--- python/ex11.py
+++ python/ex11.py
@@ -1,4 +1,4 @@
-# THIS ITERATES THROUGH THE PAIR OF A dictionary
+# THIS ITERATES THROUGH THE PAIR OF A dictionary using ".item"
 # SO.  Effecxtively ".items" turns a dictionary into a sequential list so that you can move through the items in order.
 
 states_names ={"VA":"Virginia","MD":"Maryland"}
