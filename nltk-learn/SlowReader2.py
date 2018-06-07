ecd6757792d3585533754eb1dd33ccd980848320
Sherwood <Sherwood@vagelos-ve702-5786.apng.wlan.private.upenn.edu>
2018-06-07 14:38:24 -0400

    All but word chooser funcxtions

--- nltk-learn/SlowReader2.py
+++ nltk-learn/SlowReader2.py
@@ -1,73 +1,44 @@
-import nltk, re, pprint #  unclear if needed
-# READ FILE
+import nltk, re, pprint     #  unclear if needed
+from time import sleep      # python module to use sleep, delay
+
+dur = 0.5         # set sleep variable for use in delay. see sleep() below
+s_max_len = 100 # setting global variable for max lenght of sentence
+s_max_num = 20  #    counter for how many sentences will be displayed
 
 def open_file_and_get_text(filename):
     with open(filename,'r') as our_file:
         text = our_file.read()
     return text  # takes the file, reads it. stores it as a string
 
-def sentence_printer():
+def sentence_printer(sh_sents):
     ctr = 0
-    while ctr < 5:
-        s_max_len = 100  # max of sentences ot permit
-        t_len = (len(w_sents[ctr]))
-
+    while ctr < s_max_num:
+        t_len = (len(sh_sents[ctr]))
         if ((t_len <s_max_len)):
-            print(f"Sentence number {ctr} ")
-            print(len(w_sents[ctr]))
-            print(w_sents[ctr])
+            print(sh_sents[ctr])
         ctr = ctr + 1
+        sleep(dur) # Time in seconds.
 
+def sentence_user_choice(sh_sents,key_word):
+    # key_word sthis variable is now available for use
+    ctr = 0
+    while ctr < s_max_num:
+        t_len = (len(sh_sents[ctr]))
+        if ((t_len <s_max_len)):
+            print(sh_sents[ctr])
+        ctr = ctr + 1
+        sleep(dur) # Time in seconds.
 # ----------------------------------------
+
+u_word = input("Choose one word to slow read from Walden: ")
+print("You typed", len(nltk.word_tokenize(u_word)), "words: ",u_word)
+# var u_word is now available for driving the program
 
 our_file = "walden.txt"
 text = open_file_and_get_text(our_file)
 w_words = nltk.word_tokenize(text) # nltk list for working words
 w_sents = nltk.sent_tokenize(text) # nltk list of all sentences
-sentence_printer() # calls sentence printer function
 
+# nb w_sents is a List, w_words is also a list NOT a string. sentence_printer(w_sents) # calls sentence printer function
 
-
-
-
-
-
-
-
-
-
-
-### move up later
-
-
-# w_words is now a list
-#print("These are words")
-#print(w_words[0:30]) # prints first 29 items in List
-
-# nltk_walden_text = nltk.Text(w_words)
-
-######### CONCORDANCE EXAMPLE ########
-#print(nltk_walden_text.concordance("bottom"))
-# adding variable to test it
-
-#bottom_concord = (nltk_walden_text.concordance("bottom"))
-#print(bottom_concord)
-
-
-## TESTING DISPERSION PLOT
-## NOTE THAT you need an NLTK object rather than a simple list to run nltk proesss on it.
-# COMMENT THIS NEXT LINE IF DESIRED
-
-#nltk_walden_text.dispersion_plot(['bottom','pond'])
-
-
-
-
-
-# TOKENIZE file
-
-# FORM SENTENCES - group tokens into list of sentence strings
-
-# SUB SELECT - form a list of sentences incl. "word-x"
-
-# DISPLAYER - increment through the subselect, on sentence at a Time
+sentence_user_choice(w_sents,u_word) # passes user input word to function that calculates if it is in the sentence.
