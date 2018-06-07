c75d8ac34a1012c0b1100f9366b01d224fcd2c7e
Sherwood <Sherwood@vagelos-ve702-5786.apng.wlan.private.upenn.edu>
2018-06-07 11:21:01 -0400

    Brandon Checking

--- nltk-learn/SlowReader.py
+++ nltk-learn/SlowReader.py
@@ -1,6 +1,21 @@
+import nltk, re, pprint
+# from nltk import work_tokenize # unclear if necessary
 
 
 # READ FILE
+def open_file_and_get_text(filename):
+    with open(filename,'r') as our_file:
+        text = our_file.read()
+    return text  # takes the file, reads it. stores it as a string
+
+our_file = "walden.txt"
+text = open_file_and_get_text(our_file)
+w_words = nltk.word_tokenize(text)
+# w_words is now a list
+print("These are words")
+print(w_words[0:30]) # prints first 29 items in List
+nltk_walden_text = nltk.Text(our_file)
+print(nltk_walden_text.concordance("the"))
 
 # TOKENIZE file
 
